c8e020698a953fd4e6e45d16c58936edaf708f92
Aleksandra-zimoch <115931670+Aleksandra-zimoch@users.noreply.github.com>
2023-05-23 14:48:35 +0200

    Update variables.tf

---
+++ azure-app-service/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "Nazwa planu usługi aplikacji"
+  type        = string
+}
+
+variable "app_service_plan_location" {
+  description = "Lokalizacja planu usługi aplikacji"
+  type        = string
+}

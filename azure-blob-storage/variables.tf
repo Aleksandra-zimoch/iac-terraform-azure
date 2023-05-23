be3032f01fbe6ab080635b24e20789631980f507
Aleksandra-zimoch <115931670+Aleksandra-zimoch@users.noreply.github.com>
2023-05-23 14:46:43 +0200

    Update variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Lokalizacja grupy zasobów"
+  type        = string
+}
